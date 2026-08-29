4b63afcf26786818f269a0d37e883263cb7cabba
alex <alex.podikoglou@gmail.com>
2026-08-29 23:59:05 +0300

    feat: basic incomplete parser

---
+++ src/lib.rs
@@ -0,0 +1,66 @@
+use std::collections::HashMap;
+
+use winnow::{
+    ModalResult, Parser,
+    ascii::dec_int,
+    combinator::{alt, repeat},
+    stream::AsChar,
+    token::take_while,
+};
+
+#[derive(PartialEq, Eq, Hash)]
+pub struct JsonString(String);
+
+pub struct JsonNumber(pub f32);
+pub struct JsonObject(pub HashMap<JsonString, JsonValue>);
+
+pub enum JsonValue {
+    String(JsonString),
+    Number(JsonNumber),
+    Object(JsonObject),
+}
+
+pub fn parse_string(input: &mut &str) -> ModalResult<JsonString> {
+    ('"', take_while(0.., AsChar::is_alpha), '"')
+        .map(|(_, string, _): (_, &str, _)| string.to_string())
+        .map(JsonString)
+        .parse_next(input)
+}
+
+pub fn parse_number(input: &mut &str) -> ModalResult<JsonNumber> {
+    dec_int
+        .map(|x: i32| x as f32)
+        .map(JsonNumber)
+        .parse_next(input)
+}
+
+pub fn parse_key_value(input: &mut &str) -> ModalResult<(JsonString, JsonValue)> {
+    todo!()
+}
+
+pub fn parse_object(input: &mut &str) -> ModalResult<JsonObject> {
+    (
+        "{",
+        repeat(0.., parse_key_value)
+            .fold(
+                HashMap::<JsonString, JsonValue>::new,
+                |mut acc: HashMap<JsonString, JsonValue>, item| {
+                    acc.insert(item.0, item.1);
+                    acc
+                },
+            )
+            .map(JsonObject),
+        "}",
+    )
+        .map(|(_, object, _): (_, JsonObject, _)| object)
+        .parse_next(input)
+}
+
+pub fn parse_value(input: &mut &str) -> ModalResult<JsonValue> {
+    alt((
+        parse_string.map(JsonValue::String),
+        parse_number.map(JsonValue::Number),
+        parse_object.map(JsonValue::Object),
+    ))
+    .parse_next(input)
+}
